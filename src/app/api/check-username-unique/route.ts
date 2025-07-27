ce7967d17fc01c690374c82a18057cecd9c2bb92
Siddhant Chouksey <siddnt444@gmail.com>
2025-07-27 17:41:27 +0530

    7. feat: add username uniqueness check and verification code handling

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,71 @@
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { z } from 'zod';
+import { usernameValidation } from '@/schemas/signUpSchema';
+// not wrtten more about this file. 
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParams = {
+      username: searchParams.get('username'), // get the username from the query params
+    };
+
+    const result = UsernameQuerySchema.safeParse(queryParams);  // Validate the query parameters, by running it through zod schema
+    // safeParse returns an object with success and data or error properties
+    // if success is true, then data will contain the parsed data, else error will contain the validation errors
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || []; // if there are any errors in the username validation, then it will be present in the error object
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(', ')
+              : 'Invalid query parameters',
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: 'Username is already taken',
+        },
+        { status: 200 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: 'Username is unique',
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error checking username:', error);
+    return Response.json(
+      {
+        success: false,
+        message: 'Error checking username',
+      },
+      { status: 500 }
+    );
+  }
+}
